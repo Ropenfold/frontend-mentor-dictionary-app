c76383bb50a66415a10185e2b95d9b1db4ed42b7
Stuart Roper <stuart.roper@bravissimo.com>
2025-02-21 16:57:51 +0000

    add font provider to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "next-themes";
+
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { FontProvider } from "./context/FontContext";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,9 +15,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html suppressHydrationWarning lang="en">
+      <body>
+        <FontProvider>
+        <ThemeProvider>
         {children}
+        </ThemeProvider>
+        </FontProvider>
       </body>
     </html>
   );
