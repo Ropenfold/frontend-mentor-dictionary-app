aab70469a5c99058ea13106af571267af20f180a
Stuart Roper <stuart.roper@bravissimo.com>
2025-02-23 16:48:23 +0000

    create startState module

---
+++ app/components/start-state/startState.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './startState.module.css'
+
+const StartState = () => {
+  return (
+    <div className={styles.startStateContainer}>
+      <h2 className={styles.heading}>Find your first word...</h2>
+      </div>
+  )
+}
+
+export default StartState
